aa5cdf14a3d6a2d62bbc038f9f2c4b6b432875f1
Christopher Walker <cnwalker@uchicago.edu>
2017-01-31 02:06:34 -0600

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
-      version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
+setup(name='BridgesAPIExample',
+      version='0.0.1',
+      description='API supporting virtual mentor for students w/ disabilities',
+      author='TechTeam',
+      author_email='uchicagotechteam@gmail.com',
+      url='https://github.com/uchicagotechteam',
+#      install_requires=['Django>=1.10'],
      )
