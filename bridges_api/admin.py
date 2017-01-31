4457a7e6624a634f9928aab0c4ed6605a40d86cd
Christopher Walker <cnwalker@uchicago.edu>
2017-01-31 02:04:01 -0600

    Questions are now createable via the admin interface

--- bridges_api/admin.py
+++ bridges_api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from bridges_api.models import Question
 
-# Register your models here.
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+admin.site.register(Question, QuestionAdmin)
