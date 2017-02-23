5700e54ee5ae8b00035ce7f70026dc4e48cd279c
Ivy Ma <ivyma@wireless-s0-so-150-32-176.uchicago.edu>
2017-02-22 20:04:49 -0600

    recommendations

--- bridges_api/recommendations.py
+++ bridges_api/recommendations.py
@@ -1,3 +1,46 @@
+attribute_weights * {
+    "gender":3,
+    "ethnicity":3,
+    "current_employer":10,
+    "deaf":10,
+    "blind":11,
+}
+
+def removeScoresFromList(tupleList) :
+    newList = []
+    for i in range (len(tupleList)-1,-1,-1):
+        newList.append(tupleList[i][0])
+    return newList
+
+def insertQuestion(currentQTuple, sortedQuestionList):
+    if not len(sortedQuestionList)>0 :
+        sortedQuestionList.append(currentQTuple)
+    elif (len(sortedQuestionList) <20) or (currentQTuple[1] > sortedQuestionList[0][1]) :
+        lo = 0
+        hi = len(sortedQuestionList)
+        while lo < hi:
+            mid = (lo+hi)//2
+            if currentQTuple[1] < sortedQuestionList[mid][1]:
+                hi = mid
+            else:
+                lo = mid+1
+        sortedQuestionList.insert(lo,currentQTuple)
+        while len(sortedQuestionList)>20:
+            sortedQuestionList.pop(0)
+    
 def recommend(userprofile, Question):
-    print(Question.objects.all())
-    return userprofile
+    tuplelist = []
+    for question in Question.objects.all():
+        questionscore = 0
+        for tag in question.tags.all():
+            if tag.attribute in ["gender", "current_employer"]:
+                if getattr(userprofile, tag.attribute) == tag.value:
+                    questionscore += attribute_weights[tag.value]
+            else:
+                attlist = getattr(userprofile, tag.attribute).split(str = ",", 1)
+                if tag.value in attlist:
+                    questionscore += attribute_weights[tag.value]
+        questiontuple = (question, questionscore)
+        insertQuestion(questiontuple, recommended_questions)
+    questionslist = removeScoresFromList(tuplelist)
+    return questionslist
