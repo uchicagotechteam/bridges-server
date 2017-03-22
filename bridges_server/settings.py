131d39e14d04cce40be68b9c564cec3163ef3400
Christopher Walker <cnwalker@uchicago.edu>
2017-03-22 15:14:36 -0400

    added wsgi folder and removed leading /

--- bridges_server/settings.py
+++ bridges_server/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 if os.environ.get('OPENSHIFT_REPO_DIR'):
-    STATIC_ROOT = os.environ.get('OPENSHIFT_REPO_DIR') + '/wsgi/static/'
+    STATIC_ROOT = os.environ.get('OPENSHIFT_REPO_DIR') + 'wsgi/static/'
 else:
     STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'static')
 
