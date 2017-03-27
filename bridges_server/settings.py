c18f3a79c228b036a94401ea41ba28603e6dab14
Christopher Walker <cnwalker@uchicago.edu>
2017-03-27 11:45:00 -0500

    set DEBUG mode to false and enforcing root HTTPS request via django (root python server isn't apache so .htaccess will have no effect)

--- bridges_server/settings.py
+++ bridges_server/settings.py
@@ -24,8 +24,10 @@ SECRET_KEY = 'np&)l+u$v8d38&!4+aby3c)e9=9tlm4+x!@q+&wckku8y1_eq4'
 
 if os.environ.get('OPENSHIFT_REPO_DIR'):
     # Don't want to run debug mode in prod
-    DEBUG = True
+    DEBUG = False
     ALLOWED_HOSTS = ["bridgesapi-skypath.rhcloud.com"]
+    # Redirect all HTTP requests via HTTPS
+    SECURE_SSL_REDIRECT = True
 else:
     DEBUG = True
     ALLOWED_HOSTS = ["*"]
@@ -82,7 +84,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'bridges_server.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
