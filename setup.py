13384f4e044e29ca92a9eb16ed9faa8b930c0f27
Christopher Walker <cnwalker@uchicago.edu>
2017-04-10 04:25:17 -0500

    fixed setup file

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='BridgesAPIExample',
+setup(name='Bridges API',
       version='0.0.1',
-      description='API supporting virtual mentor for students w/ disabilities',
+      description='API supporting the Employable App - a virtual mentor for students w/ disabilities',
       author='TechTeam',
       author_email='uchicagotechteam@gmail.com',
       url='https://github.com/uchicagotechteam',
-#      install_requires=['Django>=1.10'],
+      install_requires=['Django>=1.10', 'djangoresetframework==3.5.3', 'Pillow==4.0.0'],
      )
