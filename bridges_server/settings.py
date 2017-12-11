b1f5605205ca4c651fdd76c9d9af044c5333f1aa
Christopher Walker <cnwalker@uchicago.edu>
2017-12-11 00:27:01 -0600

    Add postgres print statement

--- bridges_server/settings.py
+++ bridges_server/settings.py
@@ -144,6 +144,7 @@ USE_L10N = True
 USE_TZ = True
 
 print("Repo dir: " + str(os.environ.get('OPENSHIFT_REPO_DIR')))
+print("postgres dir: " + str(os.environ.get('POSTGRESQL_USER')))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
